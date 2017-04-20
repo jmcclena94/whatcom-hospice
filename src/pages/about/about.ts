ca1c565fcdce7fc7bd7af73359313d31b17d8073
Joseph McClenahan <jmcclena94@gmail.com>
2017-04-20 11:00:08 -0700

    created initial project pages scaffold

---
+++ src/pages/about/about.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-abou',
+  templateUrl: 'abou.html'
+})
+export class AboutPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
